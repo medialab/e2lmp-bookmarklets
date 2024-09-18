ece16da73708e476d29a720a3450dedf70f11bd4
Yomguithereal <guillaumeplique@gmail.com>
2024-09-18 11:36:53 +0200

    Pretty pretty

--- template.js
+++ template.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 
-const template = fs.readFileSync("index.html", 'utf-8');
-const bookmarklet = fs.readFileSync('build/bookmarklet.js', 'utf-8').trim();
+const template = fs.readFileSync("index.html", "utf-8");
+const bookmarklet = fs.readFileSync("build/bookmarklet.js", "utf-8").trim();
 const templated = template.replace(/\{\{bookmarklet\}\}/g, bookmarklet);
 fs.writeFileSync("build/index.html", templated);
