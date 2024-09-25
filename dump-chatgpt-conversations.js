90e83443fc832510d15fe3d8dc504b1f0eb1c692
bmaz <beatrice.mazoyer@gmail.com>
2024-09-25 11:18:27 +0200

    Paginate

--- dump-chatgpt-conversations.js
+++ dump-chatgpt-conversations.js
@@ -2,32 +2,57 @@ javascript: (async function () {
   const accessToken =
     __remixContext.state.loaderData.root.clientBootstrap.session.accessToken;
 
-  const data = await fetch(
-    "https://chatgpt.com/backend-api/conversations?offset=0&limit=28&order=updated",
-    {
-      credentials: "include",
-      headers: {
-        "User-Agent":
-          "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:130.0) Gecko/20100101 Firefox/130.0",
-        Accept: "*/*",
-        "Accept-Language": "en-US,en;q=0.5",
-        "OAI-Language": "en-US",
-        // "OAI-Device-Id": "c52f08f2-853b-4517-9d54-c84632a45604",
-        Authorization: `Bearer ${accessToken}`,
-        "Sec-Fetch-Dest": "empty",
-        "Sec-Fetch-Mode": "cors",
-        "Sec-Fetch-Site": "same-origin",
-        Priority: "u=4",
-      },
-      // "referrer": "https://chatgpt.com/c/737d7a1e-96f5-4ce3-a6eb-c6567acac430",
-      method: "GET",
-      mode: "cors",
-    },
-  ).then((r) => r.json());
+  async function paginate() {
+    let results = [];
+    let offset = 0;
+    const limit = 10;
+    const apiUrl = (o) =>
+      `https://chatgpt.com/backend-api/conversations?offset=${o}&limit=${limit}&order=updated`;
+
+    while (true) {
+      console.log(apiUrl(offset));
+      const response = await fetch(apiUrl(offset), {
+        credentials: "include",
+        headers: {
+          "User-Agent":
+            "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:130.0) Gecko/20100101 Firefox/130.0",
+          Accept: "*/*",
+          "Accept-Language": "en-US,en;q=0.5",
+          "OAI-Language": "en-US",
+          // "OAI-Device-Id": "c52f08f2-853b-4517-9d54-c84632a45604",
+          Authorization: `Bearer ${accessToken}`,
+          "Sec-Fetch-Dest": "empty",
+          "Sec-Fetch-Mode": "cors",
+          "Sec-Fetch-Site": "same-origin",
+          Priority: "u=4",
+        },
+        // "referrer": "https://chatgpt.com/c/737d7a1e-96f5-4ce3-a6eb-c6567acac430",
+        method: "GET",
+        mode: "cors",
+      });
+      const data = await response.json();
+      console.log(data);
+
+      if (data.items && data.items.length > 0) {
+        results = results.concat(data.items);
+
+        offset += limit;
+
+        if (data.items.length < limit) {
+          break;
+        }
+      } else {
+        break;
+      }
+    }
+    return results;
+  }
+
+  const data = await paginate();
 
   const csvText =
     "url\ttitle\tcreate_time\tupdate_time\n" +
-    data.items
+    data
       .map((item) => {
         const url = `https://chatgpt.com/c/${item.id}`;
 
